06097d5b6e852c39fc0c59d6db3a99706e1defb5
Anto Prakash <prakashashwin124@gmail.com>
2020-10-07 19:06:44 -0500

    Manual merge with Task 1 changes.

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,11 +1,8 @@
 class HelloWorldApp {
  public static void main(String[] args) {
  System.out.println("Hello World!"); // Display the string.
-<<<<<<< HEAD
  System.out.println("Hello Universe!"); // Task 2. Be more inclusive.
  System.out.println("From Earth."); // Task 2. Be more inclusive.
-=======
- System.out.println("says George");
->>>>>>> 7ce7695ef761d1c8436b4825e16ec70154e4033a
+ System.out.println("Says George");
  }
 }
